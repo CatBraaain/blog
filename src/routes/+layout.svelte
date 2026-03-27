b74c0b90b3005f0ac9624ccf707ed4eab8449bf2
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-03-27 17:22:05 +0900

    feat: add root page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,56 +1,69 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { page } from "$app/state";
+  import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from "$lib/config";
+  import type { LayoutProps } from "./$types";
   import Header from "./Header.svelte";
-  import "./layout.css";
 
-  let { children } = $props();
+  // import "../style/global.css";
+  import "overlayscrollbars/overlayscrollbars.css";
+
+  let { children }: LayoutProps = $props();
+
+  const title = $derived(page.data.title ?? SITE_TITLE);
+  const description = $derived(page.data.description ?? SITE_DESCRIPTION);
+  const canonicalUrl = $derived(new URL(page.url.pathname, SITE_URL).href);
+  const url = $derived(new URL(page.url.href, SITE_URL).href);
+
+  const pageTitle = $derived(
+    title && title !== SITE_TITLE ? `${title} | ${SITE_TITLE}` : SITE_TITLE,
+  );
+
+  onMount(async () => {
+    const { OverlayScrollbars, ClickScrollPlugin } =
+      await import("overlayscrollbars");
+    OverlayScrollbars.plugin(ClickScrollPlugin);
+    OverlayScrollbars(document.body, {
+      scrollbars: {
+        clickScroll: true,
+      },
+    });
+  });
 </script>
 
-<div class="app">
+<svelte:head>
+  <title>{pageTitle}</title>
+  <meta name="title" content={pageTitle} />
+  {#if description}
+    <meta name="description" content={description} />
+  {/if}
+
+  <link rel="canonical" href={canonicalUrl} />
+
+  <!-- OG -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={pageTitle} />
+  {#if description}
+    <meta property="og:description" content={description} />
+  {/if}
+  <!-- TODO: add image -->
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content={url} />
+  <meta property="twitter:title" content={pageTitle} />
+  {#if description}
+    <meta property="twitter:description" content={description} />
+  {/if}
+  <!-- TODO: add image -->
+
+  <!-- TODO: add pagefind meta -->
+</svelte:head>
+
+<div class="max-w-5xl mx-auto flex flex-col gap-5 px-5 pb-5">
   <Header />
-  <main>{@render children()}</main>
-
-  <footer>
-    <p>
-      visit
-      <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
-      to learn about SvelteKit
-    </p>
-  </footer>
+  <main>
+    {@render children()}
+  </main>
 </div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import welcomeFallback from "$lib/images/svelte-welcome.png";
-  import welcome from "$lib/images/svelte-welcome.webp";
-
   import Counter from "./Counter.svelte";
 </script>
 
@@ -12,14 +9,7 @@
 
 <section>
   <h1>
-    <span class="welcome">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img src={welcomeFallback} alt="Welcome" />
-      </picture>
-    </span>
-
-    to your new<br />SvelteKit app
+    welcome to your new<br />SvelteKit app
   </h1>
 
   <h2>
