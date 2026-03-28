7465b1eff88accb177cb7fb86f3f9413468c8a2c
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-03-28 15:21:19 +0900

    feat: tweak root page

---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "site title";
+export const SITE_DESCRIPTION = "site description";
+export const SITE_URL = "https://example.com";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import Counter from "./Counter.svelte";
 </script>
 
 <svelte:head>
@@ -7,44 +6,11 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-  <h1>
-    welcome to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-
-  <Counter />
-</section>
-
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style>
+<div class:list={["flex", "gap-5"]} data-pagefind-ignore="all">
+  <section class:list={["w-1/3"]}>
+    <!-- <Search client:load {posts} /> -->
+  </section>
+  <section class:list={["w-2/3"]}>
+    <!-- <PostList client:load {posts} /> -->
+  </section>
+</div>
