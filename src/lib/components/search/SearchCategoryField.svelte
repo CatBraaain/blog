5c6a01fafe81831079b8cd58f8cbed9e46f28efb
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-03-31 19:21:28 +0900

    refactor: remove unused condition

--- src/lib/components/search/SearchCategoryField.svelte
+++ src/lib/components/search/SearchCategoryField.svelte
@@ -10,49 +10,45 @@
   import MdiFolderOutline from "~icons/mdi/folder-outline";
 </script>
 
-{#if CATEGORY_KINDS.length > 0}
-  <Field>
-    <FieldLabel for="categories">Categories</FieldLabel>
-    <ToggleGroup
-      type="single"
-      value={SearchQuery.category}
-      variant={"default"}
-      size={"default"}
-      spacing={1}
-      class="flex flex-col items-start gap-2"
-    >
-      {#each CATEGORY_KINDS as category}
-        <ToggleGroupItem value={category}>
-          {#snippet child({ props })}
-            <a
-              href={SearchQuery.buildMergedHref({
-                category: SearchQuery.category === category ? "" : category,
-                tag: "",
-              })}
-              data-active={SearchQuery.category === category}
-              {...props}
-              class={linkVariants({
-                variant: "button",
-                size: "auto",
-                class:
-                  "flex w-full items-center justify-between text-foreground/90",
-              })}
-            >
-              <div class="icon-set">
-                <MdiFolderOutline className="accent-icon-8" />
-                {category}
-              </div>
-              <!-- <div class="normal-icon-8">
-                {
-                  (query?.word ? searchResult : posts).filter(
-                    (post) => post.category === category,
-                  ).length
-                }
-              </div> -->
-            </a>
-          {/snippet}
-        </ToggleGroupItem>
-      {/each}
-    </ToggleGroup>
-  </Field>
-{/if}
+<Field>
+  <FieldLabel for="categories">Categories</FieldLabel>
+  <ToggleGroup
+    type="single"
+    value={SearchQuery.category}
+    variant={"default"}
+    size={"default"}
+    spacing={1}
+    class="flex flex-col items-start gap-2"
+  >
+    {#each CATEGORY_KINDS as category}
+      <ToggleGroupItem value={category}>
+        {#snippet child({ props })}
+          <a
+            href={SearchQuery.buildMergedHref({
+              category: SearchQuery.category === category ? "" : category,
+              tag: "",
+            })}
+            data-active={SearchQuery.category === category}
+            {...props}
+            class={linkVariants({
+              variant: "button",
+              size: "auto",
+              class:
+                "flex w-full items-center justify-between text-foreground/90",
+            })}
+          >
+            <div class="icon-set">
+              <MdiFolderOutline className="accent-icon-8" />
+              {category}
+            </div>
+            <!-- <div class="normal-icon-8">
+              {(query?.word ? searchResult : posts).filter(
+                (post) => post.category === category,
+              ).length}
+            </div> -->
+          </a>
+        {/snippet}
+      </ToggleGroupItem>
+    {/each}
+  </ToggleGroup>
+</Field>
