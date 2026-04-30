b47b8d99018e4f4c2abb1635292d8f2eab784121
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-04-30 13:31:00 +0900

    chore: add tagging script

---
+++ scripts/tagging.ts
@@ -0,0 +1,26 @@
+import { Glob } from "bun";
+import { metaPatterns } from "../src/lib/meta-rules.ts";
+
+const glob = new Glob("content/**/*.md");
+for await (const file of glob.scan(".")) {
+  const text = await Bun.file(file).text();
+  const matchedMetaRules = [
+    ...new Set(
+      metaPatterns.filter((metaPattern) => {
+        const langMatch = metaPattern.langPattern ? metaPattern.langPattern.test(text) : false;
+        const titleMatch = metaPattern.titlePattern ? metaPattern.titlePattern.test(text) : false;
+        const bodyMatch = metaPattern.bodyPattern ? metaPattern.bodyPattern.test(text) : false;
+        return langMatch || titleMatch || bodyMatch;
+      }),
+    ),
+  ];
+  const tags = matchedMetaRules
+    .filter((metaMetaRule) => metaMetaRule.tag)
+    .map((metaMetaRule) => metaMetaRule.tag)
+    .filter((tagName, _, tagNames) => !(tagName === "windows" && tagNames.includes("autohotkey")))
+    .sort();
+  const tagsString = `tags: [${tags.map((t) => `"${t}"`).join(", ")}]`;
+  const category = `category: ${matchedMetaRules.find((metaRule) => metaRule.category)?.category ?? "Life"}`;
+
+  await Bun.write(file, text.replace(/^tags:.*$/m, tagsString).replace(/^category:.*$/m, category));
+}
