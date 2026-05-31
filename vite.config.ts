44cacb0b5db2600cdca7ef3654a6372ea99ce876
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-05-31 21:36:41 +0900

    refactor: use vite-plugin-md2svelte package from npmjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
 import Icons from "unplugin-icons/vite";
+import { md2svelte } from "vite-plugin-md2svelte";
 import { defineConfig } from "vitest/config";
-import { md2svelte } from "./plugins/vite-plugin-md2svelte";
+import { rehypeCodeBlock } from "./plugins/rehype-code-block";
+import { remarkFenced } from "./plugins/remark-fenced";
+import { postMetaSchema } from "./src/lib/post-meta";
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    md2svelte(),
+    md2svelte({
+      components: { CodeBlock: "$lib/components/CodeBlock.svelte" },
+      remarkPlugins: [remarkBreaks, remarkFenced, remarkGfm],
+      rehypePlugins: [rehypeCodeBlock],
+      frontmatterSchema: postMetaSchema,
+    }),
     sveltekit(),
     Icons({
       compiler: "svelte",
